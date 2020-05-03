757798064fbc5fd9b4d6c8cce248c793012eacfe
James Shelton <jshelton140@googlemail.com>
2020-05-03 18:05:29 +0100

    added printMenu to Output

--- src/Output.java
+++ src/Output.java
@@ -1,8 +1,16 @@
 
 public class Output {
 
-	public Output() {
-		// TODO Auto-generated constructor stub
+	public static void printMenu(String[] menu) {
+		//System.out.println(Arrays.toString(menu));
+		
+		for(int i = 0; i < menu.length; i++) {
+			if(menu[i] != "Exit") {
+				System.out.println(i+1+". "+menu[i]);
+			} else {
+				System.out.println(0+". "+menu[i]);
+			}
+		}
 	}
 
 	public static void main(String[] args) {
