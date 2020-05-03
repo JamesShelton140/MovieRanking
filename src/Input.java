15e6850fb45f4938d8eb4a95d8f966d12378f570
James Shelton <jshelton140@googlemail.com>
2020-05-03 17:38:02 +0100

    Create IO classes

--- src/Input.java
+++ src/Input.java
@@ -1,14 +1,13 @@
-import java.util.*;
 
-public class MovieRanking {
+public class Input {
 
-	public MovieRanking() {
+	public Input() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+
 	}
 
 }
